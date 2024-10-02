36232476b90aa3a10ad639d239feda0a6abe4dcb
BiskySheed <raashidus@gmail.com>
2024-10-02 11:39:36 -0700

    List of organizations added

--- garagefinder/lib/main.dart
+++ garagefinder/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart'; // Assuming this package exists
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +8,112 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return ShadApp.cupertino(
+      cupertinoThemeBuilder: (context, theme) {
+        return theme.copyWith(applyThemeToAll: true);
+      },
+      materialThemeBuilder: (context, theme) {
+        return theme.copyWith(
+          appBarTheme: const AppBarTheme(toolbarHeight: 52),
+        );
+      },
+      home: const HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Garage Finder Homepage'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: OrganizationSelector(),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  class OrganizationSelector extends StatelessWidget {
+  OrganizationSelector({super.key});
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final organizations = {
+    'Northern California': [
+      'CSU Chico',
+      'CSU East Bay',
+      'Humboldt State',
+      'CSU Maritime',
+      'CSU Sacramento',
+      'San Francisco State',
+      'Sonoma State',
+    ],
+    'Central California': [
+      'CSU Fresno',
+      'CSU Monterey Bay',
+      'Cal Poly San Luis Obispo',
+      'CSU Stanislaus',
+      'San Jose State',
+    ],
+    'Southern California': [
+      'CSU Bakersfield',
+      'CSU Channel Islands',
+      'CSU Dominguez Hills',
+      'CSU Fullerton',
+      'CSU Los Angeles',
+      'CSU Long Beach',
+      'CSU Northridge',
+      'Cal Poly Pomona',
+      'CSU San Marcos',
+      'CSU San Bernardino',
+      'San Diego State',
+    ],
+  };
+
+  List<Widget> getOrganizationsWidgets(ShadThemeData theme) {
+    final widgets = <Widget>[];
+    for (final region in organizations.entries) {
+      widgets.add(
+        Padding(
+          padding: const EdgeInsets.fromLTRB(32, 6, 6, 6),
+          child: Text(
+            region.key,  // Displays the region name
+            style: theme.textTheme.muted.copyWith(
+              fontWeight: FontWeight.w600,
+              color: theme.colorScheme.popoverForeground,
+            ),
+            textAlign: TextAlign.start,
+          ),
+        ),
+      );
+      widgets.addAll(region.value
+          .map((organization) => ShadOption(value: organization, child: Text(organization))));
+    }
+    return widgets;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final theme = ShadTheme.of(context); // Ensure you fetch theme appropriately
+
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minWidth: 280),
+      child: ShadSelect<String>(
+        placeholder: const Text('Select an organization'),
+        options: getOrganizationsWidgets(theme),
+        selectedOptionBuilder: (context, value) {
+          return Text(value!);
+        },
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
